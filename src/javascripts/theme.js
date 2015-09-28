646c84a70bf2ce59ac889d5f69271b8ee7594b0b
Marcelo Borges <borgespereira.marcelo@gmail.com>
2015-09-28 16:48:45 -0300

    Adding some new files, folders, and doing improvements on general styling

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -3,3 +3,17 @@ window.jQuery = $;
 window.$ = $;
 
 var bootstrap = require('bootstrap');
+
+var summary = $('.summary');
+
+$(window).scroll(function(){
+	var viewableOffset = $(".summary").offset().top - $(window).scrollTop()
+	//var viewableOffset = $('.summary').offset().top;
+	if (viewableOffset <=30) {
+		summary.css("position", "fixed");
+		//summary.css("top", "40");
+	}
+	else {
+		summary.css("position", "relative");
+	}
+});
