d29e3140b0bc5f4b69d0df71306ca109722f9cf1
Marcelo Borges <borgespereira.marcelo@gmail.com>
2015-09-29 09:39:51 -0300

    Solving issue hor-19 and some of the problems in issue 7 of the zertico-theme

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -5,15 +5,3 @@ window.$ = $;
 var bootstrap = require('bootstrap');
 
 var summary = $('.summary');
-
-$(window).scroll(function(){
-	var viewableOffset = $(".summary").offset().top - $(window).scrollTop()
-	//var viewableOffset = $('.summary').offset().top;
-	if (viewableOffset <=30) {
-		summary.css("position", "fixed");
-		//summary.css("top", "40");
-	}
-	else {
-		summary.css("position", "relative");
-	}
-});
