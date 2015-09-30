762595a0c4b370575165ca2f739eb4cf1af480ca
Marcelo Borges <borgespereira.marcelo@gmail.com>
2015-09-30 16:13:08 -0300

    Changing the html structure, translating everything to english, fixing breadcrumbs, and some modifications to general styling

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -4,4 +4,27 @@ window.$ = $;
 
 var bootstrap = require('bootstrap');
 
-var summary = $('.summary');
+//ACTIVATE DROPDOWN ON ADMIN SIDEBAR
+$('#side-menu').children('li').children('a').click(function() {
+	if($(this).parents('.active').length) {
+		$(this).parent().removeClass('active');
+		$('.collapse').collapse("hide");
+	}
+	else {
+		$('#side-menu li').removeClass('active');
+    	$('.collapse').collapse("hide");
+
+    	$(this).parent().addClass('active');
+	    $(this).next().collapse("show");
+
+    	return false;
+    	e.preventDefault();
+	}
+});
+
+//HIDE AND SHOW ADMIN LEFT MENU
+$('.show-menu').click(function() {
+	$('#wrapper').toggleClass('toggled');
+});
+
+//$.getScript("your-javascript.js")
