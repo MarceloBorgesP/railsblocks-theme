06910af68f1e97a12f6b333060232fb8a5f04bb9
Marcelo Borges <borgespereira.marcelo@gmail.com>
2015-10-01 16:30:13 -0300

    Translating some PT to EN, Fixing links and Removing unnecessary js

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -4,27 +4,13 @@ window.$ = $;
 
 var bootstrap = require('bootstrap');
 
-//ACTIVATE DROPDOWN ON ADMIN SIDEBAR
-$('#side-menu').children('li').children('a').click(function() {
-	if($(this).parents('.active').length) {
-		$(this).parent().removeClass('active');
-		$('.collapse').collapse("hide");
-	}
-	else {
-		$('#side-menu li').removeClass('active');
-    	$('.collapse').collapse("hide");
+var init = function () {
+	//HIDE AND SHOW ADMIN LEFT MENU
+	$('.show-menu').click(function() {
+		$('#wrapper').toggleClass('toggled');
+	});
+}
 
-    	$(this).parent().addClass('active');
-	    $(this).next().collapse("show");
-
-    	return false;
-    	e.preventDefault();
-	}
-});
-
-//HIDE AND SHOW ADMIN LEFT MENU
-$('.show-menu').click(function() {
-	$('#wrapper').toggleClass('toggled');
-});
+$(document).ready(init);
 
 //$.getScript("your-javascript.js")
